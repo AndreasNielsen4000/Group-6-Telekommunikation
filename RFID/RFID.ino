76cf8987ec1b624659bd567fe139f0400f6454cf
lukasschou <lukasschou@gmail.com>
2024-01-12 11:43:59 +0100

    ex32

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -3,12 +3,22 @@
 #include <Servo.h>
 #include <ESP_EEPROM.h>
 
+#include "api_caller.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+
 #define SS_PIN D8
 #define RST_PIN D0
 #define buzzer D1
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
 Servo myservo;
+
+const char *ssid = "";   /* Your SSID */
+const char *pass = "";   /* Your Password */
+const char *apiUrl = ""; /* Your API URL. Example: "https://api.example.com" */
 
 unsigned long previousMillis = 0;  // Will store last time the RIFD was read
 unsigned long currentMillis;
@@ -18,7 +28,20 @@ const int keycard_length = 11;
 String UID[keycards];  // Create placeholders for the RFID tags
 
 void setup() {
-  Serial.begin(115200);                     // Initiate a serial communication
+  Serial.begin(115200);  // Initiate a serial communication
+
+  WiFi.begin(ssid, pass);
+  // Waiting for WIFI connection
+  Serial.println("WIFI connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n\nConnected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
   EEPROM.begin(keycards * keycard_length);  // Initiate EEPROM memory size
   SPI.begin();                              // Initiate  SPI bus
   mfrc522.PCD_Init();                       // Initiate MFRC522
@@ -49,34 +72,75 @@ void setup() {
 }
 
 void loop() {
-  // Look for new cards
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-  // Select one of the cards
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
+  while (WiFi.status() != WL_CONNECTED) {
+    //Serial.println("Not connected to WiFi...");
+    //delay(1000);  // remove delay
 
-  // Only read the UID if it is over the interval length since it has last been read
-  currentMillis = millis();
-  if (currentMillis - previousMillis > interval) {
-    bool access_granted = false;
-
-    for (int i = 0; i < keycards; i++) {
-      if (UID[i] == read_RFID()) {
-        access_tone();
-        access_granted = true;
-      }
-      Serial.println(UID[i]);
+    // Look for new cards
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+      return;
+    }
+    // Select one of the cards
+    if (!mfrc522.PICC_ReadCardSerial()) {
+      return;
     }
 
-    if (access_granted == false) {
+    // Only read the UID if it is over the interval length since it has last been read
+    currentMillis = millis();
+    if (currentMillis - previousMillis > interval) {
+      bool access_granted = false;
+
+      for (int i = 0; i < keycards; i++) {
+        if (UID[i] == read_RFID()) {
+          access_tone();
+          access_granted = true;
+        }
+        Serial.println(UID[i]);
+      }
+
+      if (access_granted == false) {
+        no_access_tone();
+      }
+
+      String active_UID = read_RFID();
+      Serial.println(active_UID);
+    }
+  }
+
+  // Create a WiFiClient object to talk to the server.
+  WiFiClient client;
+
+  // Create an ApiCaller object with the WiFiClient and API URL.
+  ApiCaller apiCaller(client, apiUrl);
+
+  // Call the API with a pin
+  String UID = read_RFID();
+  std::unique_ptr<DynamicJsonDocument> doc = apiCaller.get("/rfid", UID);
+
+  // Check if the API call was successful.
+  if (doc == nullptr) {
+    Serial.println("Error calling API");
+    delay(1000);
+    return;
+  }
+  if (doc->containsKey("authenticated")) {
+    if (doc->get<bool>("authenticated")) {
+      Serial.println("API call was successful and the pin was correct.");
+
+      access_tone();
+
+      // Get the value of the "message" key.
+      // TODO: check if the key exists before getting the value.
+      String message = doc->get<String>("message");
+      Serial.println(message);
+    } else {
+      Serial.println("API call was successful, but the pin was incorrect.");
       no_access_tone();
     }
-
-    String active_UID = read_RFID();
-    Serial.println(active_UID);
+  } else {
+    Serial.println("API call was successful, but the response was not valid.");
+    delay(1000);
+    return;
   }
 }
 
---
+++ api_caller/example/api_caller.cpp
@@ -0,0 +1,121 @@
+#include "api_caller.h"
+#include <iostream>
+#include <string>
+#include <ArduinoJson.h>
+
+/*
+ * Constructor for ApiCaller class.
+ * Initializes the class with a reference to a WiFiClient and the API URL.
+ * @param client Reference to a WiFiClient object.
+ * @param apiUrl URL of the API to be called. Example: "http://api.example.com"
+ */
+ApiCaller::ApiCaller(WiFiClient &client, String apiUrl) : client(client), apiUrl(apiUrl) // Initializer member variables
+{
+}
+
+/*
+ * Makes a call to a specified endpoint of the API with a given value.
+ * Constructs the full URL, sends the request, and processes the response.
+ *
+ * @param endpoint The specific endpoint of the API to be called.
+ * @param value The value to be passed to the endpoint.
+ * @return A unique_ptr to a DynamicJsonDocument containing the response, or nullptr if an error occurs.
+ */
+std::unique_ptr<DynamicJsonDocument> ApiCaller::call(String endpoint, String value)
+{
+// TODO: check if there is a "/" in between all values
+#include <string>
+
+    String url = this->format_url(this->apiUrl + "/" + endpoint + "/" + value);
+
+    Serial.print("Calling API: ");
+    Serial.println(url);
+
+    HTTPClient https;
+
+    https.begin(client, url.c_str());
+    https.addHeader("Content-Type", "application/json");
+
+    int httpCode = https.GET();
+    if (httpCode <= 0)
+    {
+        Serial.println("Error on HTTP request");
+        return nullptr;
+    }
+
+    String payload = https.getString();
+    // TODO: figure out how big DynamicJsonDocument should be
+    std::unique_ptr<DynamicJsonDocument> doc = std::make_unique<DynamicJsonDocument>(payload.length() * 2);
+
+    // cleanup the https connection
+    https.end();
+
+    // deserialize the JSON payload
+    DeserializationError jsonError = deserializeJson(*doc, payload);
+
+    switch (jsonError.code())
+    {
+    case DeserializationError::Code::Ok:
+        Serial.println("Deserialization of JSON was successful");
+        return doc;
+    case DeserializationError::Code::EmptyInput:
+        Serial.println("Error: Empty input to JSON deserialization");
+        return nullptr;
+    case DeserializationError::Code::IncompleteInput:
+        Serial.println("Error: Incomplete JSON input");
+        return nullptr;
+    case DeserializationError::Code::InvalidInput:
+        Serial.println("Error: Invalid JSON input");
+        return nullptr;
+    case DeserializationError::Code::NoMemory:
+        Serial.println("Error: No memory");
+        return nullptr;
+    case DeserializationError::Code::TooDeep:
+        Serial.println("Error: JSON object too deep");
+        return nullptr;
+    default:
+        Serial.println("Error: Unknown error");
+        return nullptr;
+    }
+}
+
+/*
+ * Formats a URL by removing any double slashes.
+ * @param url The URL to be formatted.
+ * @return The formatted URL.
+ */
+String ApiCaller::format_url(String url)
+{
+    // TODO: maybe use std::string instead of String
+    // Convert the String to a std::string
+    std::string urlStr(url.begin(), url.end());
+
+    // Find the position of "http://" or "https://"
+    size_t httpsPos = urlStr.find("https://");
+    size_t httpPos = urlStr.find("http://");
+    size_t startPos;
+
+    // Determine the starting position for replacements
+    if (httpsPos != std::string::npos)
+    {
+        startPos = httpsPos + 8; // Length of "https://"
+    }
+    else if (httpPos != std::string::npos)
+    {
+        startPos = httpPos + 7; // Length of "http://"
+    }
+    else
+    {
+        startPos = 0; // Start from beginning if neither is found
+    }
+
+    // Loop through the string and replace "//" with "/"
+    size_t pos = startPos;
+    while ((pos = urlStr.find("//", pos)) != std::string::npos)
+    {
+        urlStr.replace(pos, 2, "/");
+        pos++; // Move past the replaced "/"
+    }
+
+    return urlStr.c_str();
+}
